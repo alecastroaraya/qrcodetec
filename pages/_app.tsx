169c8ceb93a936028a112025a963fa598a5ae494
alejandrocastroarayaitcr <90380164+alecastroitcr@users.noreply.github.com>
2023-03-19 01:25:30 -0600

    bootstrap agregado

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.css";
+
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+  useEffect(() => {
+    typeof document !== undefined
+      ? require("bootstrap/dist/js/bootstrap")
+      : null;
+  }, [router.events]);
+
+  return <Component {...pageProps} />;
 }
