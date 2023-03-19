67588f4b7ac9eb85ba79ae2a08889a8962207ccd
alejandrocastroarayaitcr <90380164+alecastroitcr@users.noreply.github.com>
2023-03-19 12:58:33 -0600

    estructura general de agregar codigo qr

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,33 +26,104 @@ export default function Home() {
         </nav>
       </div>
 
-      <div className="container-md position-absolute top-50">
-        <h1>Generador de códigos QR</h1>
-      </div>
-
-      <div className="container-lg position-relative">
-
+      <div className="container-lg">
+      <p className="text-center text-info"><h1><u>Generador de códigos QR</u></h1></p>
         <div className="card">
 
-          <div className="row">
-            <div className="col-md">
-            a
-            <hr className="hr" />
+          <div className="row-sm bg-info">
+
+            <div className="container-md">
+              <div className="col-md d-flex p-2 justify-content-center">
+                  <p className="text-center text-white"><h3>Nombre de la página</h3></p>
+              </div>
             </div>
           </div>
 
           <div className="row">
             <div className="col-md">
-              One of three columns
+
+              <div className="d-flex p-2 justify-content-start">
+                <button className="btn">
+                  <img src="/search-icon.png" width="30" />
+                </button>
+              </div>
             </div>
 
             <div className="col-md">
-              One of three columns
+              
+            <form method="GET" className="d-flex p-2 justify-content-between">
+              <input id="search" className="form-control me-2" type="search" placeholder="Buscar página" aria-label="Search" />
+            </form>
+
             </div>
             
             <div className="col-md">
-              One of three columns
+
+            <div className="d-flex p-2 justify-content-end">
+              <button className="btn btn-info text-white">Registrar</button>
             </div>
+
+            </div>
+          </div>
+
+          <hr className="hr" />
+          <div className="row">
+            <div className="col-md">
+
+              <div className="d-flex p-2 justify-content-start">
+                <p className="text-center text-black"><h3>Instituto de Educación Integral</h3></p>
+              </div>
+            </div>
+            
+            <div className="col-md">
+
+            <div className="d-flex p-2 justify-content-end">
+              <button className="btn">
+                <img src="/search-icon.png" width="30" />
+              </button>
+
+              <button className="btn">
+                <img src="/stats-icon.png" width="30" />
+              </button>
+
+              <button className="btn">
+                <img src="/trash-icon.png" width="30" />
+              </button>
+            </div>
+
+            </div>
+            
+
+          </div>
+          
+          <hr className="hr" />
+          <div className="row">
+            <div className="col-md">
+
+              <div className="d-flex p-2 justify-content-start">
+                <p className="text-center text-black"><h3>Nubesinos</h3></p>
+              </div>
+            </div>
+            
+            <div className="col-md">
+
+            <div className="d-flex p-2 justify-content-end">
+              <button className="btn">
+                <img src="/search-icon.png" width="30" />
+              </button>
+
+              <button className="btn">
+                <img src="/stats-icon.png" width="30" />
+              </button>
+
+              <button className="btn">
+                <img src="/trash-icon.png" width="30" />
+              </button>
+            </div>
+
+            </div>
+            
+
           </div>
 
         </div>
