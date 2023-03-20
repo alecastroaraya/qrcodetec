cca6148d322acafabc05b5f5a67b65e46c3aa9e8
alejandrocastroarayaitcr <90380164+alecastroitcr@users.noreply.github.com>
2023-03-19 18:54:31 -0600

    pagina de agregar codigos qr agregada

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,7 @@ export default function Home() {
       <main className={styles.main}>
 
       <div className="container-fluid">
-        <nav className="navbar navbar-expand-lg navbar-light fixed-top bg-light border-top border-dark border-bottom border-dark border-left border-dark border-right border-dark">
+        <nav className="navbar navbar-expand-lg navbar-light fixed-top bg-white border-top border-dark border-bottom border-dark border-left border-dark border-right border-dark">
             <a href="/" className="navbar-brand">
               <Image src="/iei_logo.jpg" width={100} height={100} alt="" className="d-inline-block align-middle mr-2"/>
               <a className="navbar-brand" href="/"><b>       Instituto de Educación Integral</b></a>
@@ -30,10 +30,10 @@ export default function Home() {
         <br/>
         <br/>
         <br/>
-        <p className="text-center text-info"><h1><u>Generador de códigos QR</u></h1></p>
-        <div className="card">
+        <p className="home_text text-center text-primary"><h1><u>Generador de códigos QR</u></h1></p>
+        <div className="card border-top border-primary border-bottom border-primary border-left border-primary border-right border-primary rounded">
 
-          <div className="row-sm bg-info">
+          <div className="row-sm bg-primary">
 
             <div className="container-md">
               <div className="col-md d-flex p-2 justify-content-center">
@@ -43,27 +43,29 @@ export default function Home() {
           </div>
 
           <div className="row">
-            <div className="col-md">
+            <div className="col-1">
 
-              <div className="d-flex p-2 justify-content-start">
-                <button className="btn">
-                  <img src="/search-icon.png" width="30" />
+              <div className="d-flex p-2 justify-content-end">
+                <button className="btn text-right">
+                  <img src="/search-icon.png" width="40" />
                 </button>
               </div>
             </div>
 
-            <div className="col-md">
-              
-            <form method="GET" className="d-flex p-2 justify-content-between">
-              <input id="search" className="form-control me-2" type="search" placeholder="Buscar página" aria-label="Search" />
-            </form>
+            <div className="col-10">
+
+              <div className="container-fluid">
+                <form method="GET" className="d-flex p-2 justify-content-between">
+                <input id="search" className="form-control me-2 input-large" type="search" placeholder="Buscar página" aria-label="Search" />
+                </form>
+              </div>
 
             </div>
             
-            <div className="col-md">
+            <div className="col-1">
 
             <div className="d-flex p-2 justify-content-end">
-              <button className="btn btn-info text-white">Registrar</button>
+              <button className="btn btn-lg btn-primary text-white">Registrar</button>
             </div>
 
             </div>
@@ -71,10 +73,15 @@ export default function Home() {
 
           <hr className="hr" />
           <div className="row">
+
+            <div className ="col-1">
+
+            </div>
+
             <div className="col-md">
 
               <div className="d-flex p-2 justify-content-start">
-                <p className="text-center text-black"><h3>Instituto de Educación Integral</h3></p>
+                <p className="text-left text-black"><h3>Instituto de Educación Integral</h3></p>
               </div>
             </div>
             
@@ -82,15 +89,15 @@ export default function Home() {
 
             <div className="d-flex p-2 justify-content-end">
               <button className="btn">
-                <img src="/search-icon.png" width="30" />
+                <img src="/download-icon.png" width="55" />
               </button>
 
               <button className="btn">
-                <img src="/stats-icon.png" width="30" />
+                <img src="/stats-icon.png" width="55" />
               </button>
 
               <button className="btn">
-                <img src="/trash-icon.png" width="30" />
+                <img src="/trash-icon.png" width="55" />
               </button>
             </div>
 
@@ -101,10 +108,14 @@ export default function Home() {
           
           <hr className="hr" />
           <div className="row">
+
+            <div className ="col-1">
+              
+            </div>
             <div className="col-md">
 
               <div className="d-flex p-2 justify-content-start">
-                <p className="text-center text-black"><h3>Nubesinos</h3></p>
+                <p className="text-left text-black"><h3>Nubesinos</h3></p>
               </div>
             </div>
             
@@ -112,15 +123,15 @@ export default function Home() {
 
             <div className="d-flex p-2 justify-content-end">
               <button className="btn">
-                <img src="/search-icon.png" width="30" />
+                <img src="/download-icon.png" width="55" />
               </button>
 
               <button className="btn">
-                <img src="/stats-icon.png" width="30" />
+                <img src="/stats-icon.png" width="55" />
               </button>
 
               <button className="btn">
-                <img src="/trash-icon.png" width="30" />
+                <img src="/trash-icon.png" width="55" />
               </button>
             </div>
 
