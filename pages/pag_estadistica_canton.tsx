3ab38b9bd351d96b44497117d827e429cb6ba784
IssaAC00 <algueracmp@gmail.com>
2023-03-25 21:16:50 -0600

    vista del grafico
    No funcional , solo primer estilo para dimensionar

--- pages/pag_estadistica_canton.tsx
+++ pages/pag_estadistica_canton.tsx
@@ -1,12 +1,53 @@
 import Head from 'next/head'
+import React from 'react';
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import { Doughnut } from 'react-chartjs-2';
+import {Chart, ArcElement} from 'chart.js'
+Chart.register(ArcElement);
 
 const inter = Inter({ subsets: ['latin'] })
 
 
 export default function pag_estadistica_canton() {
+
+    const data = {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [
+          {
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+              'red',
+              'blue',
+              'green',
+              'pink',
+              'orange',
+              'yellow',
+            ],
+            borderColor: [
+                'red',
+                'blue',
+                'green',
+                'pink',
+                'orange',
+                'yellow',
+            ],
+            borderWidth: 1,
+          },
+        ],
+      };
+
+      const Doughnut1: React.FC = () => {
+        return (
+          <div>
+            <Doughnut data={data}  />
+          </div>
+        );
+      };
+
+
     return (
         <>
         {/* Título del tab de la página */}
@@ -25,23 +66,68 @@ export default function pag_estadistica_canton() {
             <a className="header item"><h1>Instituto de Educación Integral</h1></a>
         </div>
 
-        {/* Título del encabezado de la página */}
-        <div className="ui container">
-            <center><h1 className="ui header header-text"><u>Generador de códigos</u></h1></center>
+            
+    {/* Linebreaks para formato de la página, ignorar */}
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>   
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+
+    {/* Título del encabezado de la página */}
+    <div className="ui container">
+    <center><h1 className="ui header header-text"><u>Generador de códigos</u></h1></center>
+    </div>
+
+    {/* Inicio del cuadro con la información principal de la página */}
+
+    {/* Fila con el nombre de la sección de la página, en este caso dice nombre de la página porque es la homepage */}
+    <br/>
+    <div className="ui container border-1">
+    <div className="ui container fluid">
+    <center>
+    <div className="ui inverted segment">
+    <h2>Estadísticas por cantón</h2>
+    </div>
+    </center>
+    </div>
+
+    {/* Estas filas contienen la información respectiva de cada página, en este caso por ser la homepage contienen la información de los códigos QR */}
+    <div className="ui vertically divided bottom aligned grid">
+    <div className="four column row">
+    <div className="one wide column">
+
+    {/* Botón búsqueda de código QR */}
+    </div>
+
+    {/* Botón para registrar un código QR */}
+
+    </div>
+
+    {/* En esta sección se comienza a mostrar todos los códigos QR y sus botones respectivos */}
+    <div className="four column row">
+    <div className="one wide column">
+
+    </div>
+        <div>
+        <Doughnut1 />
         </div>
+    
+    <div className="two wide column">
+        <button className="ui medium button">
+        <h3>Regresar</h3>
+        </button>
+    </div>
 
-        <br/>
-            <div className="ui container border-1">
-                <div className="ui container fluid">
-                <center>
-                <div className="ui inverted segment">
-                    <h2>Nombre de la página</h2>
-                </div>
-                </center>
-                </div>
-            </div>
+    </div>
 
+    </div>
 
+    </div>
         </>
 
         
