3119d97dfb6e56d607b2c1a26c582285940b5a88
IssaAC00 <algueracmp@gmail.com>
2023-03-27 19:37:42 -0600

    estadistica vista nada mpas completa

--- pages/pag_estadistica_canton.tsx
+++ pages/pag_estadistica_canton.tsx
@@ -11,7 +11,9 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function pag_estadistica_canton() {
 
-    const data = {
+//codigo para uso del grafico 
+
+/*     const data = {
         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
         datasets: [
           {
@@ -50,7 +52,7 @@ export default function pag_estadistica_canton() {
           </div>
         );
       };
-
+ */
 
 
     return (
@@ -90,34 +92,46 @@ export default function pag_estadistica_canton() {
 
     {/* Inicio del cuadro con la información principal de la página */}
 
-    {/* Fila con el nombre de la sección de la página, en este caso dice nombre de la página porque es la homepage */}
-    <br/>
-    <div className="ui container border-1">
-    <div className="ui container fluid">
-    <center>
-    <div className="ui inverted segment">
-    <h2>Estadísticas por cantón</h2>
-    </div>
-    </center>
-    </div>
-    <br/>
-    <br/>
-    <br/>
-    <center>
-    <div  style={{height: '50%' , width: '50%'}} >
-        <Doughnut1 />
-    </div>
-    </center>
-    <div className="one wide column">
-      <button className="ui small button">
-      <a href="/"><h3>Regresar</h3></a>
-      </button>
-    </div>
+        {/* Fila con el nombre de la sección de la página, en este caso dice nombre de la página porque es la homepage */}
+        <br />
+      <div className="ui container border-1">
+        <div className="ui container fluid">
+          <center>
+            <div className="ui inverted segment">
+              <h2>Estadísticas por cantón </h2>
+            </div>
+          </center>
+        </div>
 
-    </div>
-        </>
+        {/* Estas filas contienen la información respectiva de cada página, en este caso muestra el gráfico del código QR respectivo */}
+        <div className="ui vertically divided bottom aligned grid">
+          <div className="fifteen column row">
+            <div className="two wide column">
 
-        
+            </div>
 
+            <div className="nine wide column">
+              {/* <Doughnut1  /> */}
+              <img src="/grafico_ejemplo.png" />
+            </div>
+
+
+            <div className="five wide column">
+              <div className="middle aligned header">
+                <h2>Número total de vistas</h2>
+                <h2>---------------------------------</h2>
+              </div>
+              <button className="ui medium button">
+                <a href="/">
+                  <h3>Regresar</h3>
+                </a>
+              </button>
+              <br />
+              <br />
+            </div>
+          </div>
+        </div>
+      </div>
+     </>
     )
 }
