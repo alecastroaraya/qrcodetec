a108f198b052792e55e41f70185b41005d2cc584
Luis Diego Mora Aguilar <39139078+LuisDi98@users.noreply.github.com>
2023-04-29 00:17:54 -0600

    Fixed URL Generator to redirect to server

--- pages/api/addQR.js
+++ pages/api/addQR.js
@@ -1,18 +1,37 @@
 import clientPromise from "../../lib/mongodb";
-const QRCode = require('qrcode');
 
+/*
+req es un objeto que contiene información sobre la solicitud HTTP 
+  que disparó el evento de la función.
+
+res es un objeto que contiene información sobre la respuesta HTTP 
+  que se enviará cuando finalice la función.
+*/
 export default async (req, res) => {
   try {
     const client = await clientPromise;
+    // Nos dice cual es la base de datos de mongo que usaremos, en este caso "qrcodes"
     const db = client.db("qrcodes");
-    const { name, description, url } = req.body;
+    /////////////////////////////////////////////////////////////////////////
+    
+      // AQUI, en esta parte podemos crear el url nuevo para que 
+      // envie al usuario a nuestro servidor y no a la pagina de qrcode
 
+    // Obtenemos los valores que ingreso el usuario en el formulario
+    const urlDelServidor = "http://localhost:3000/qr/readQR?qrUrl=";
+    let { name, description, url } = req.body;
+    // Creamos el url nuevo para que envie al usuario a nuestro servidor y no a la pagina de qrcode
+    url = urlDelServidor + url;
+
+    /////////////////////////////////////////////////////////////////////////
+
+    // Le hacemos INSERT del QR nuevo a la base de datos
     const qrCode = await db.collection("qrcodes").insertOne({
       name,
       description,
       url,
     });
-
+    // Regresamos al frontend el QR que acabamos de crear
     res.json(qrCode);
   } catch (e) {
     console.error(e);
